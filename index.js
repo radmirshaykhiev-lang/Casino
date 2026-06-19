637958c8ffedcfb1bbf5b2217b871840ca083026
Zentical_75 <radmirshaykhiev@gmail.com>
2026-06-20 00:25:47 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const win = 4;
 let money = 10;
 var ArkashaDengi = "ZOVGOIDA52421488"
 
+document.getElementById('but67').hidden = true;
 function smeshnueKartinkiSmotretOnlainBezRegistrachii(){
     if (money >= 50){
         money = money - 50;
         document.getElementById("balance").textContent = "Баланс: " + money + " " + ArkashaDengi;
         window.open("smeshnieKartinki.html", "_blank");
+    }
+}
+
+function f67(){
+    for(i = 0; i<=67; i++){
+        window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     }
 }
 
@@ -22,7 +29,22 @@ function clikDengiPlusPlus(){
         return;
     }
     money++;
+    if (money == 5 || money == -5 || money == -6 || money == 6 || money == 7 || money == -7 || money == -8 || money == 8 || money == 9 || money == -9  || money == 11 || money == 10 || money == 12 || money == 13 || money == 14 || money == 15 || money == 16 || money == 17 || money == 18 || money == 19 || lastNomer(money) == 5 || lastNomer(money) == 6 || lastNomer(money) == 7 || lastNomer(money) == 8 || lastNomer(money) == 9){
+        ArkashaDengi = "Аркадий-коинов" 
+    }
+    if(money == 1 || money == -1 || lastNomer(money) == 1 ){
+        ArkashaDengi = "Аркадий-коин"
+    }else if(money == 2 || money ==-2 || lastNomer(money) == 2 || money == 3 || money ==-3 || lastNomer(money) == 3 || money == 2 || money ==-4 || lastNomer(money) == 4){
+        ArkashaDengi = "Аркадий-коина"
+    }
     document.getElementById("balance").textContent = "Баланс: " + money + " " + ArkashaDengi;
+    if(money == 67){
+        document.getElementById('but67').hidden = false;
+    }
+    else
+    {
+        document.getElementById('but67').hidden = true;
+    }
 }
 function lastNomer(number){
     return Math.abs(number) % 10;
@@ -53,9 +75,20 @@ function play() {
 
 
     document.getElementById("balance").textContent = "Баланс: " + money + " " + ArkashaDengi;
+    if(money == 67){
+        document.getElementById('but67').hidden = false;
+    }
+    else
+    {
+        document.getElementById('but67').hidden = true;
+    }
 }
 function re(){
     money = 10
      document.getElementById("balance").textContent = "Баланс: " + money + " " + ArkashaDengi;
 
 }
+//document.getElementById('but67').hidden = 'true';
+//if (money == 50){
+//    document.getElementById('but67').style.display = 'false';
+//}
